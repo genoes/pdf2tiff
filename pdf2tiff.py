801d0964f836f2aa33065e49b651b92a45f65a88
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2021-03-26 13:07:44 -0700

    adding py file

---
+++ pdf2tiff.py
@@ -0,0 +1,28 @@
+from pdf2image import convert_from_path
+import os, shutil
+try:
+    os.makedirs('./TIFFs')
+except FileExistsError:
+    # directory already exists
+    pass
+
+pdf_file = input('\n'"Enter absolute path to PDF: ").strip(" ")
+pages = convert_from_path(pdf_file)
+img_file = pdf_file.replace(".pdf", "")
+
+count = 0
+for page in pages:
+    count += 1
+    tiff_file = img_file + "_" + str(count).zfill(3) + ".tif"
+    page.save(tiff_file), 'TIFF'
+    shutil.move(tiff_file, './TIFFs')
+
+print('\n'
+''' ,-~~-.___.
+ / |  '     \    "Finished!"
+(  )         0
+ \_/-, ,----'
+    ====           //
+   /  \-'~;    /~~~(O)
+  /  __/~|   /       |
+=(  _____| (_________|''''\n')
