b17af693c61deca78b6cd7004350c1c47606e332
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2021-08-03 14:18:31 -0700

    updated script to include progress info/set DPI

--- batch_process/pdf2tiff.py
+++ batch_process/pdf2tiff.py
@@ -1,4 +1,5 @@
 from pdf2image import convert_from_path
+from tqdm import tqdm
 import os, shutil
 try:
     os.makedirs('./TIFFs')
@@ -6,20 +7,22 @@ except FileExistsError:
     # directory already exists
     pass
 
-pdf_directory = input('\n''Enter absolute path to PDF directory: ').strip()
+pdf_directory = input('\n'"Enter absolute path to PDF directory: ").strip()
+
 for filename in os.listdir(pdf_directory):
     if filename.endswith('.pdf'):
         file = (os.path.join(pdf_directory, filename))
         pages = convert_from_path(file)
-        img_file = file.replace(".pdf", "")
+        img_file = file.replace('.pdf', "")
 
         count = 0
-        for page in pages:
+        pbar = tqdm(pages, colour = 'green', bar_format = '{l_bar}{bar:20}{r_bar}{bar:-10b}')
+        for page in pbar:
+            pbar.set_description("Extracting TIFFs from %s" % filename)
             count += 1
             tiff_file = img_file + "_" + str(count).zfill(3) + ".tif"
-            page.save(tiff_file), 'TIFF'
+            page.save(tiff_file, dpi=(600, 600)), 'TIFF'
             shutil.move(tiff_file, './TIFFs')
-        print("creating TIFFs from", os.path.basename(file))
 
 print('\n'
 '''
